73dad1155ec52dcf0118346c43377064c1a7f708
li.tong3 <li.tong3@northeastern.edu>
2021-03-21 23:05:56 -0700

    Updated Assignment

--- src/ArticleListItem/ArticleListItem.jsx
+++ src/ArticleListItem/ArticleListItem.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import ArticleTextToggleButton from "../ArticleTextToggleButton/ArticleTextToggleButton";
+import styles from "./ArticleListItem.module.css";
 
 const ArticleListItem = (props) => {
-  return (
-    <li>
-      <article>
-        <h2>{props.article.title}</h2>
-        <time dateTime={props.article.timeStamp}>
-          {props.article.displayDate}
-        </time>
-        <p>{props.article.shortText}</p>
-      </article>
-    </li>
-  );
-};
+  const [showInfo, setShowInfo] = useState(false);
+  const [buttonText, setButtonText] = useState("Show more");
 
-ArticleListItem.propTypes = {
-  article: PropTypes.object.isRequired,
-};
-export default ArticleListItem;
+  const onClick = () => {
+    setShowInfo(!showInfo);
+    if(!showInfo) {
+      setButtonText("Show less");
+    } else {
+      setButtonText("Show more");
+    }
+  };
+
+  return (
+      <li className={styles.item}>
+        <article className={styles.article}>
+          <h2 className={styles.title}>{props.article.title}</h2>
+          {showInfo &&
+            <div>
+              <p>{props.article.shortText}</p>
+              <time className={styles.dateTime} dateTime={props.article.timeStamp}>{props.article.displayDate}</time>
+            </div>
+          }
+        </article>
+        <ArticleTextToggleButton onClick={onClick} buttonText={buttonText}/>
+      </li>
+    );
+  };
+
+  ArticleListItem.propTypes = {
+    article: PropTypes.object.isRequired
+  };
+
+  export default ArticleListItem;
